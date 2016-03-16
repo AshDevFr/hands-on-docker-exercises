18e47152332a12274353936a1440cfff8d665200
Sylvain CAU <ashdevfr@gmail.com>
2016-03-16 22:31:27 +0100

    :art: Add infos

--- Part_4-Real_Case/Exercise2_Solution/src/server.js
+++ Part_4-Real_Case/Exercise2_Solution/src/server.js
@@ -6,7 +6,7 @@
 
   http.createServer((request, response) => {
     response.writeHead(200, {'Content-Type' : 'text/plain'});
-    response.end('Hello World\n');
+    response.end('Hello World\nPart4: Exercise2 Solution');
   }).listen(port);
 
   console.log('Server running at http://127.0.0.1:' + port + '/');
